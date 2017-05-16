7c79a3266ff80297ea00795eb6b2a677738f83d3
spiderbatman <gigi.pataraia@gmail.com>
2017-05-16 23:05:09 +0400

    is tipi prosta dadis isrebit

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,50 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', 
+	{
+		preload: preloader, 
+		create: create, 
+		update: update
+	}
+);
+function preloader() {
+	game.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+	cursors = game.input.keyboard.createCursorKeys();
+}
+	
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	dude = game.add.sprite(0, 0, 'dude');
+	game.physics.arcade.enable(dude);
+	dude.body.collideWorldBounds = true;
+	dude.animations.add('left', [0, 1, 2 ,3], 10, true);
+	dude.animations.add('right', [5, 6, 7, 8], 10, true);
+}
 
-// Add States
-game.state.add('Preloader', Spliter.Preloader);
-game.state.add('Menu',      Spliter.Menu);
-game.state.add('Game',      Spliter.Game);
-
-// Run first state
-game.state.start('Preloader');
+function update() {
+	dude.body.velocity.x = 0;
+	dude.body.velocity.y = 0;
+    if (cursors.left.isDown)
+    {
+        dude.body.velocity.x = -150;
+        dude.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        dude.body.velocity.x = 150;
+        dude.animations.play('right');
+    }
+    else if (cursors.up.isDown) {
+    	dude.body.velocity.y = -150;
+    	dude.animations.play('left');
+    }
+    else if (cursors.down.isDown) {
+    	dude.body.velocity.y = 150;
+    	dude.animations.play('right');	
+    }
+    else
+    {
+        dude.animations.stop();
+        dude.frame = 4;
+ 	}
+}
+var dude;
+var cursors;
